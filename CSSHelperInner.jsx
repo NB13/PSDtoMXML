965bcd3949c0f24e81252cda58c4e74e6d65fc18
Alexander Metelsky <metelsky@belkatechnologies.com>
2014-10-03 12:30:01 +0300

    layers border out from document fixed

--- CSSHelperInner.jsx
+++ CSSHelperInner.jsx
@@ -1,4 +1,13 @@
-﻿var justificationMapping = {
+﻿var a = {};
+
+   a["Ё"]="YO";a["Й"]="I";a["Ц"]="TS";a["У"]="U";a["К"]="K";a["Е"]="E";a["Н"]="N";a["Г"]="G";a["Ш"]="SH";a["Щ"]="SCH";a["З"]="Z";a["Х"]="H";a["Ъ"]="'";
+   a["ё"]="yo";a["й"]="i";a["ц"]="ts";a["у"]="u";a["к"]="k";a["е"]="e";a["н"]="n";a["г"]="g";a["ш"]="sh";a["щ"]="sch";a["з"]="z";a["х"]="h";a["ъ"]="'";
+   a["Ф"]="F";a["Ы"]="I";a["В"]="V";a["А"]="a";a["П"]="P";a["Р"]="R";a["О"]="O";a["Л"]="L";a["Д"]="D";a["Ж"]="ZH";a["Э"]="E";
+   a["ф"]="f";a["ы"]="i";a["в"]="v";a["а"]="a";a["п"]="p";a["р"]="r";a["о"]="o";a["л"]="l";a["д"]="d";a["ж"]="zh";a["э"]="e";
+   a["Я"]="Ya";a["Ч"]="CH";a["С"]="S";a["М"]="M";a["И"]="I";a["Т"]="T";a["Ь"]="'";a["Б"]="B";a["Ю"]="YU";
+   a["я"]="ya";a["ч"]="ch";a["с"]="s";a["м"]="m";a["и"]="i";a["т"]="t";a["ь"]="'";a["б"]="b";a["ю"]="yu";
+
+var justificationMapping = {
     "Justification.CENTER": 'center',
     "Justification.CENTERJUSTIFIED": 'center',
     "Justification.FULLYJUSTIFIED": 'justify',
@@ -14,34 +23,39 @@ app.preferences.typeUnits = TypeUnits.POINTS
 var doc = app.activeDocument;
 
 if (doc) {
-    var output = {};
+    var output = {items:[]};
     var docName = doc.name.match(/([^\.]+)/)[1];
     var targetFolder = new Folder( doc.path + "/" + docName);    
     targetFolder.create();     
     
     foreachLayerRecursive(doc, function (layer) {
-        doc.activeLayer = layer;
         var data;
-        var iterateChildren = true;
-        if (String(layer.typename) == "LayerSet") {
-            if (layer.name.indexOf("button") == 0) {
-                data = {type: "button", parameters: parseButton(layer)};
-                iterateChildren = false;
-            }
-        } else {
-            if (layer.kind == LayerKind.TEXT) {
-                data = { type: 'text', parameters: parseTextLayer(layer)};
-            } else {
-                data = { type: 'sprite', parameters: parseSpriteLayer(layer)};
-            }
-        }
-        if (!(data === undefined)) {
-            data.x = layer.bounds[0].value;
-            data.y = layer.bounds[1].value;
-            data.width = layer.bounds[2].value - data.x;
-            data.height = layer.bounds[3].value - data.y;
-            putLayerData(output, layer, data);
-        }
+        var iterateChildren = false;
+		if( layer.visible == true ){
+			doc.activeLayer = layer;
+			iterateChildren = true;
+			if (String(layer.typename) == "LayerSet") {
+				if (layer.name.indexOf("button") == 0) {
+					data = {type: "button", parameters: parseButton(layer)};
+					iterateChildren = false;
+				}
+			} else {
+				if (layer.kind == LayerKind.TEXT) {
+					data = { type: 'text', parameters: parseTextLayer(layer)};
+				} else {
+					data = { type: 'sprite', parameters: parseSpriteLayer(layer)};
+				}
+			}
+			if (!(data === undefined)) {
+				data.x = layer.bounds[0].value;
+				data.y = layer.bounds[1].value;
+				data.width = layer.bounds[2].value - data.x;
+				data.height = layer.bounds[3].value - data.y;
+				putLayerData(output, layer, data);
+			} else{ 
+				putLayerData(output, layer, {});
+			}
+		}
         return iterateChildren;
     });
    
@@ -58,37 +72,41 @@ function putLayerData(output, layer, data) {
         currentLayer = currentLayer.parent;
         stack.push(transformLayerName(currentLayer.name));
     }
-    var currentOutput = output;
+    var currentOutput = output.items;
     for (var i = stack.length - 1; i > -1; i--) {
-        if (currentOutput[stack[i]] === undefined) {
-            currentOutput[stack[i]] = {};
-        }
-        currentOutput = currentOutput[stack[i]];
+		for( var j = 0; j < currentOutput.length; j++ ){
+			if( currentOutput[j].id == stack[i]  ){
+				break;
+			}
+		}
+		if( currentOutput.length > j && currentOutput[j].id == stack[i]  ){
+			currentOutput = currentOutput[j].items;
+		} else{
+			currentOutput.push( {id:stack[i], items:[]});
+			currentOutput = currentOutput[currentOutput.length - 1].items;
+		}
     }
-    currentOutput[transformLayerName(layer.name)] = data;
+	currentOutput.push({id:transformLayerName(layer.name), data:data, items:[]});
 }
 
 function transformLayerName(name) {
-    return name.replace(/ /g, '_');
+    name = name.replace(/ /g, '_');
+    var result = '';
+     for(var i = 0; i < name.length; i++) {
+        var c = name.charAt(i);
+        result += a[c] || c;
+    }
+    return result;
 }
 
 function foreachLayerRecursive(doc, action) {
-    var iterateChildren = foreachLayer(doc.layerSets, action);
-    for (var i = 0; i < doc.layerSets.length; i++) {
-        if (iterateChildren[i]) {
-            foreachLayerRecursive(doc.layerSets[i], action);
+    if( doc.layers ){    
+        for (var i = 0; i < doc.layers.length; i++) {
+            if( action(doc.layers[i])){
+                foreachLayerRecursive (doc.layers[i], action)
+            }
         }
     }
-    foreachLayer(doc.artLayers, action);
-}
-
-function foreachLayer(layers, action) {
-    var iterateChildren = [];
-    for (var i = 0; i < layers.length; i++) {
-        var iterateThisLayerChildren = action(layers[i]);
-        iterateChildren.push(iterateThisLayerChildren);
-    }
-    return iterateChildren;
 }
 
 function layerFullName(layer) {
@@ -131,12 +149,21 @@ function parseTextLayer(layer) {
 function parseSpriteLayer(layer) {
     var doc = app.activeDocument;
 
+	doc.activeLayer = layer;
+	
+	if( !layer.isBackgroundLayer){
+		layer.rasterize(RasterizeType.ENTIRELAYER);
+		rasterizeLayerStyle();
+		doc.crop(new Array(0,0,doc.width,doc.height));
+	}
+	
     var initialState = doc.activeHistoryState;
     doc.activeLayer = layer;
     
     var newDocument = app.documents.add( doc.width, doc.height, doc.resolution, "temp", NewDocumentMode.RGB, DocumentFill.TRANSPARENT); 
     newDocument.activeLayer.isBackgroundLayer = false;
     app.activeDocument = doc;
+	
     layer.duplicate(newDocument);
     app.activeDocument = newDocument;
     newDocument.trim(TrimType.TRANSPARENT);
@@ -176,4 +203,21 @@ function getChildLayerByName(layer, name){
             return layer.layers[i];
         }
     }
+}
+
+function rasterizeLayerStyle(){
+    var idrasterizeLayer = stringIDToTypeID( "rasterizeLayer" );
+    var desc5 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref4 = new ActionReference();
+        var idLyr = charIDToTypeID( "Lyr " );
+        var idOrdn = charIDToTypeID( "Ordn" );
+        var idTrgt = charIDToTypeID( "Trgt" );
+        ref4.putEnumerated( idLyr, idOrdn, idTrgt );
+    desc5.putReference( idnull, ref4 );
+    var idWhat = charIDToTypeID( "What" );
+    var idrasterizeItem = stringIDToTypeID( "rasterizeItem" );
+    var idlayerStyle = stringIDToTypeID( "layerStyle" );
+    desc5.putEnumerated( idWhat, idrasterizeItem, idlayerStyle );
+    executeAction( idrasterizeLayer, desc5, DialogModes.NO );
 }
